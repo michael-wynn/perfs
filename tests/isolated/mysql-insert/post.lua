88b90a82cfb0bb0d3bc9ca7ed317019c78c9f010
michael-wynn <vrcmwynn@hotmail.com>
2015-10-07 01:14:14 -0700

    refine

--- tests/isolated/mysql-insert/post.lua
+++ tests/isolated/mysql-insert/post.lua
@@ -1,4 +1,4 @@
 wrk.method = "POST"
 wrk.path = "/mysql-insert"
 wrk.headers["content-type"] = "application/json"
-wrk.body = io.input("one-rows.json"):read("*a")
+wrk.body = io.input("ten-rows.json"):read("*a")
